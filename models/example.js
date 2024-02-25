28d3c5f363d04e8435c476968d7db8ad209d37fb
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-02-25 13:30:37 +0700

    feat: setup project structure

---
+++ models/example.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const exampleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Example = mongoose.model('Example', exampleSchema);
+
+module.exports = Example;
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-const express = require('express')
-const app = express()
+const dotenv = require('dotenv');
+const express = require('express');
+const connectDB = require('./config/db');
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+// load env vars
+dotenv.config({ path: './config/config.env' });
+
+// connect DB
+connectDB();
+
+const app = express();
+// body parser
+app.use(express.json());
+
+// routes
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
+
+// handle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // close server & exit process
+    app.close(() => process.exit(1));
+});
