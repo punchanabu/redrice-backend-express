e1373fe2f52de9c52262e525b972109a67811009
punchpnp <nunnapatppp@gmail.com>
2024-02-27 21:38:41 +0700

    User login : create method for check matching (password/email)

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,3 +25,44 @@ exports.register = async (req, res, next) => {
         console.log(err.stack);
     }
 };
+
+//@desc  Login user
+//@route POST /api/v1/auth/register
+//@access Public
+// เงื่อนไขการ match ของ email และ password
+exports.login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email & password
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({
+                success: false,
+                msg: 'Please provide an email and password',
+            });
+    }
+
+    // check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res
+            .status(400)
+            .json({ success: false, msg: 'Invalid credentials' });
+    }
+
+    // check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return res
+            .status(401)
+            .json({ success: false, msg: 'Invalid credentials' });
+    }
+
+    // create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
+};
