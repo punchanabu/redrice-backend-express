1cc7e0038c9489528103c09d25960970614b122d
punchpnp <nunnapatppp@gmail.com>
2024-02-27 20:58:46 +0700

    Post register

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,21 @@ const User = require('../models/User');
 //@desc  Register user
 //@route POST /api/v1/auth/register
 //@access Public
-exports.register = (req, res, next) => {
-    res.status(200).json({ success: true });
+exports.register = async (req, res, next) => {
+    try {
+        const { name, email, telephone, password, role } = req.body;
+
+        // create user
+        const user = await User.create({
+            name,
+            email,
+            telephone,
+            password,
+            role,
+        });
+        res.status(200).json({ success: true });
+    } catch (err) {
+        res.status(400).json({ success: false });
+        console.log(err.stack);
+    }
 };
