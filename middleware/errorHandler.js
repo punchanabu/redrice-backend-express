81d3fca48f62fb8aeeeb6b712b9650d95247fff9
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-01 02:09:27 +0700

    feat(errorHandler): handle the mongoose error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,13 +1,31 @@
 // This function will handle all the errors that are thrown in the application
 const ErrorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    
-    const statusCode = err.statusCode || 500; 
-    const errorMessage = err.message || 'Internal Server Error';
+    console.error(err);
 
-    res.status(statusCode).json({
+    let err = { ...err };
+    err.message = err.message;
+
+    // Mongoose bad ObjectId
+    if (err.name === 'CastError') {
+        const message = `Resource not found with id of ${err.value}`;
+        err = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map((value) => value.message);
+        err = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = `Duplicate field value entered`;
+        err = new ErrorResponse(message, 400);
+    }
+
+    res.status(statusCode || 500).json({
         success: false,
-        error: errorMessage,
+        error: errorMessage || 'Server Error',
     });
 };
 
