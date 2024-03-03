0c1482f2bd78f20cfcc550166b2a6601a714e3b7
punchpnp <nunnapatppp@gmail.com>
2024-03-03 19:02:02 +0700

    fix: route problem

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -13,12 +13,12 @@ const { protect, authorize } = require('../middleware/auth');
 
 router
     .route('/')
-    .get(protect, getRestaurants)
+    .get(getRestaurants)
     .post(protect, authorize('admin'), createRestaurant);
 
 router
     .route('/:id')
-    .get(protect, getRestaurant)
+    .get(getRestaurant)
     .put(protect, authorize('admin'), updateRestaurant)
     .delete(protect, authorize('admin'), deleteRestaurant);
 
